7f6ea30f21edd1b0ec626d7fbe7061e58183464d
calebkabaya <calebkabaya@gmail.com>
2022-05-14 12:53:34 +0300

    added the model for login

---
+++ app/models.py
@@ -0,0 +1,47 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
+from datetime import datetime
+from flask_login import UserMixin,current_user
+
+
+
+class User(UserMixin,db.Model):
+    __tablename__='users'
+
+    id= db.Column(db.Integer,primary_key=True)
+    username= db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'User{self.username}'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
