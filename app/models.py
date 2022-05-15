b0dd4d69772aac30c1098a3a21c322aba92b6175
calebkabaya <calebkabaya@gmail.com>
2022-05-15 08:17:19 +0300

    added the models class for commenting

--- app/models.py
+++ app/models.py
@@ -16,6 +16,8 @@ class User(UserMixin,db.Model):
     pass_secure = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     blogs = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+
 
 
     @property
@@ -53,6 +55,8 @@ class Blog(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     time = db.Column(db.DateTime, default = datetime.utcnow)
     category = db.Column(db.String(255), index = True,nullable = False)
+    comment = db.relationship('Comment',backref='blog',lazy='dynamic')
+
     
     def save_p(self):
         db.session.add(self)
@@ -60,4 +64,25 @@ class Blog(db.Model):
 
         
     def __repr__(self):
-        return f'Blog {self.post}'    
+        return f'Blog {self.post}'   
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text(),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+    blog_id = db.Column(db.Integer,db.ForeignKey('blogs.id'),nullable = False)
+
+    def save_c(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,pitch_id):
+        comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+
+        return comments
+
+    
+    def __repr__(self):
+        return f'comment:{self.comment}'         
