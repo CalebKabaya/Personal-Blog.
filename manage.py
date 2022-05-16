c379869f2866e3abb5134c950f02c53a65efe4ee
calebkabaya <calebkabaya@gmail.com>
2022-05-16 14:47:32 +0300

    addd the uniittest

--- manage.py
+++ manage.py
@@ -13,7 +13,6 @@ from  flask_migrate import Migrate, MigrateCommand
 app = create_app('production')
 
 
-app = create_app('test')
 
 manager = Manager(app)
 manager.add_command('server',Server)
@@ -22,12 +21,12 @@ migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
 
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+# @manager.command
+# def test():
+#     """Run the unit tests."""
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_content():
